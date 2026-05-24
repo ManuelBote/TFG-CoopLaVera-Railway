ce136ef3529e80c32e521e4eb619ffd7f7e0df5c
ManuelBote <mbotez01@educarex.es>
2026-05-24 11:11:20 +0200

    BACK | ApiEntregas arreglado

--- CooperativaLaVera/app/Http/Controllers/GestionEntregasController.php
+++ CooperativaLaVera/app/Http/Controllers/GestionEntregasController.php
@@ -153,7 +153,7 @@ class GestionEntregasController extends Controller
 
             $entregas = EntregaProducto::where('id_usuario', $u->id)
                 ->join('productos', 'entrega_producto.id_producto', '=', 'productos.id')
-                ->select('productos.nombre', 'fecha_entrega', 'cantidad_total')
+                ->select('productos.nombre', 'fecha_entrega', 'cantidad_total', 'entrega_producto.estado')
                 ->orderBy('fecha_entrega', 'asc')
                 ->get();
 
@@ -162,6 +162,7 @@ class GestionEntregasController extends Controller
                 $agrupado[$entrega->nombre][] = [
                     'fecha'    => $entrega->fecha_entrega,
                     'cantidad' => $entrega->cantidad_total,
+                    'estado'   => $entrega->estado,
                 ];
             }
 
